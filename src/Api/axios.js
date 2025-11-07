b900b1f76a512a0cf3d4d3f4b156a59edbfc6bee
Kharade-Shivani <shivanikharade18@gmail.com>
2025-11-07 16:00:41 +0530

    ddf

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // export const baseURL= ' https://megatex-backend.onrender.com';
 // export const baseURL= 'http://192.168.0.202:8585';
 // export const baseURL= 'https://megatex-backend.onrender.com';
-export const baseURL= 'https://megatex-backend-02y1.onrender.com';
+export const baseURL= 'https://megatex-backend-02y1.onrender.com/';
 
 
 
